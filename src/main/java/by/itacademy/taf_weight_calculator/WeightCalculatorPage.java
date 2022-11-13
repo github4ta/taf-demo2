b9d7ff9d2c822a9d37dc49ab95d042388f0bd11e
Viktor Marchuk <viktor.m77.77@mail.ru>
2022-11-13 18:31:53 +0300

    add fields to class WeightCalculatorPage according task

--- src/main/java/by/itacademy/taf_weight_calculator/WeightCalculatorPage.java
+++ src/main/java/by/itacademy/taf_weight_calculator/WeightCalculatorPage.java
@@ -1,11 +1,11 @@
 package by.itacademy.taf_weight_calculator;
 
 public class WeightCalculatorPage{
-    private String xPathName = "//input[@name='name']";
-    private String xPathHeight = "//input[@name='height']";
-    private String xPathWeight = "//input[@name='weight']";
+    private String xPathName = "//table[@border='0']/tbody/tr[2]/td[1]";
+    private String xPathHeight = "//table[@border='0']/tbody/tr[3]/td[1]";
+    private String xPathWeight = "//table[@border='0']/tbody/tr[4]/td[1]";
     private String xPathGender = "//table[@border='0']/tbody/tr[5]/td[1]";
-    private String xPathMan = "//input[@value='m']";
-    private String xPathFemale = "//input[@value='f']";
+    private String xPathMan = "//tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]";
+    private String xPathFemale = "//tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]";
     private String xPathNameBottomTable = "//tr[@height='50']/td";
 }
