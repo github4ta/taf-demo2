9b32a8fddc218f0d5b3d0b7532015c62cc3871e6
Aliaksandr Viartseika <aliaksandr_viartseika@leverx.com>
2022-11-14 17:03:54 +0100

    refactor WeightCalculatorPage class locators

--- src/main/java/by/itacademy/taf_weight_calculator/WeightCalculatorPage.java
+++ src/main/java/by/itacademy/taf_weight_calculator/WeightCalculatorPage.java
@@ -5,6 +5,12 @@ public class WeightCalculatorPage{
     public final String A_LITTLE_MORE_MASS = "Умеренный избыток массы тела";
     public final String TOO_MUCH_MASS = "Значительный избыток массы тела, тучность";
     public final String TOO_LITTLE_MASS = "Слишком малая масса тела";
+    private String xPathInputName = "//input[@name='name']";
+    private String xPathInputHeight = "//input[@name='height']";
+    private String xPathInputWeight = "//input[@name='weight']";
+    private String maleRadioButton = "//input[@type='radio' and @value='m']";
+    private String femaleRadioButton = "//input[@type='radio' and @value='f']";
+    private String calculateWeightButton = "//input[@type='submit']";
     private String xPathName = "//table[@border='0']/tbody/tr[2]/td[1]";
     private String xPathHeight = "//table[@border='0']/tbody/tr[3]/td[1]";
     private String xPathWeight = "//table[@border='0']/tbody/tr[4]/td[1]";
