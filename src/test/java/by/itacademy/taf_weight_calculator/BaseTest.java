4802043b2dcf1a754f74008ad44cd63a3b2f70cc
Aliaksandr Viartseika <aliaksandr_viartseika@leverx.com>
2022-11-15 16:29:25 +0100

    refactor WeightCalculatorPage

--- src/test/java/by/itacademy/taf_weight_calculator/BaseTest.java
+++ src/test/java/by/itacademy/taf_weight_calculator/BaseTest.java
@@ -2,21 +2,21 @@ package by.itacademy.taf_weight_calculator;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
     WebDriver driver;
 
-    @BeforeEach
+    @Before
     public void setWebDriver() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
     }
 
-    @AfterEach
+    @After
     public void closeWebDiver() {
         driver.quit();
     }
