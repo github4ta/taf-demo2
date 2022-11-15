139d43f26a13e5aed642d7438b7b7c568097369a
hanna_klimasheuskaya <hanna.klimasheuskaya@gmail.com>
2022-11-15 17:41:09 +0300

    add testWeightWithLessValue method

--- src/test/java/by/itacademy/taf_weight_calculator/WeightCalculatorTest.java
+++ src/test/java/by/itacademy/taf_weight_calculator/WeightCalculatorTest.java
@@ -1,4 +1,19 @@
 package by.itacademy.taf_weight_calculator;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class WeightCalculatorTest extends BaseTest{
+   @Test
+    void testWeightWithLessValue(){
+       WeightCalculatorPage page = new WeightCalculatorPage(driver);
+       page.openCalculatorPage();
+       page.typeInputName("Maria");
+       page.typeInputHeight("150");
+       page.typeInputWeight("42");
+       page.typeInputGender(Gender.FEMALE);
+       page.clickSubmitButton();
+
+      Assertions.assertEquals(page.TOO_LITTLE_MASS, page.getResultMessage());
+   }
 }
